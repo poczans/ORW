83c08e73893ca4a88e65e3f8740e83c060cbdf88
Szymon <poczans@gmail.com>
2016-02-29 20:19:43 +0100

    orw-laborki

---
+++ ORW-lab/orw2/ConsoleApplication3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (WebClient wb = new WebClient())
+            {
+                try
+                {
+                    wb.Proxy = null;
+                    wb.DownloadString("http://www.onet.pl");
+                    foreach (string name in wb.ResponseHeaders.Keys)
+                    {
+                        Console.WriteLine(name + "=" + wb.ResponseHeaders[name]);
+                    }
+                }
+                catch (WebException e)
+                {
+                    Console.WriteLine(e.Status);
+                }
+            }
+        }
+    }
+}
